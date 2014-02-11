9d9d6d9d337ffe3dc4ea8f70bd69beb5fbb21639
jona <jonathan@dhcp-jonathans-air.sinner-schrader.de>
2014-02-11 09:44:45 +0100

    add a static pages controller

---
+++ rails_projects/exercises/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Exercises::Application.config.secret_key_base = '3f6802e2d0352b34a21a375571b85a5941de6d8ba2d006137f9ee46d1f380c4ad5737da0c8f27659865a2664dd41da1db724e0caad0c02f3da17a13a3a61e0c5'
